75d918000ab6295997407838647adf5bef319988
AnibalArenas <anibal@wonderbrands.co>
2023-01-17 15:01:02 -0600

    [UPD] Arregla la función volumen de product_template

--- wb_product/models/product_template.py
+++ wb_product/models/product_template.py
@@ -402,10 +402,11 @@ class ProductTemplate(models.Model):
             self.vat_price=0.00
 
     #Function that print the volume of product
-    @api.depends('product_width','product_height','product_length')
+    @api.depends('product_width', 'product_height', 'product_length')
     def _volumen(self):
-        self.ensure_one()
-        if self.product_width > 0 and self.product_height > 0 and self.product_length > 0:
-            self.product_volume = round( (self.product_width * self.product_height * self.product_length) / 5000,2)
-        else:
-            self.product_volume = 0.00
+        _logger = logging.getLogger(__name__)
+        for rec in self:
+            if rec.product_width > 0 and rec.product_height > 0 and rec.product_length > 0:
+                rec.product_volume = round((rec.product_width * rec.product_height * rec.product_length) / 5000, 2)
+            else:
+                rec.product_volume = 0.00
