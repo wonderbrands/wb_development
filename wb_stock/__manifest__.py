0528d302e12974719673acfe396bec88b93af78e
William Colin <williamcolin954@gmail.com>
2023-02-22 23:39:03 -0600

    [ADD] New function for pick process
    función para asignar la zona en los picks

--- wb_stock/__manifest__.py
+++ wb_stock/__manifest__.py
@@ -14,9 +14,6 @@
     'author': "Wonderbrands",
     'website': "https://www.wonderbrands.co",
     'license': 'LGPL-3',
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Inventory',
     'version': '15.0',
 
@@ -28,7 +25,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'views/stock_picking_views.xml',
         'views/views.xml',
         'views/templates.xml',
--- wb_stock/models/stock_picking.py
+++ wb_stock/models/stock_picking.py
@@ -7,7 +7,6 @@ from datetime import datetime
 import logging
 import json
 import requests
-from io import StringIO, BytesIO
 
 class Picking(models.Model):
     _inherit = 'stock.picking'
@@ -15,6 +14,12 @@ class Picking(models.Model):
     fecha_cita_almc = fields.Datetime(string='Fecha cita en almacén')#, compute='make_invisible', store=True)
     # leadtime = fields.Integer(string='Leadtime', help='Tiempo de entrega estimado del proveedor')
     invisible_field = fields.Boolean(string='Es IN?', help='Un campo de ayuda al programador para saber si el movimiento de almacén es un IN')
+
+    # Dev for new picking process
+    pick_priority = fields.Selection([('0', '0'), ('1', '1'), ('2', '2')],
+                             default='0', string='Priority level', help='Field that allows to enter the priority level of pick', store=True)
+    pick_zone = fields.Many2one('stock.location', string='Pick zone', help='Field that allows to choose a stock location, this field is set from the first line of the stock move line', compute='_zone_assignment', index=True) #Campo relacionado con el modelo de Ubicaciones
+    priority_check = fields.Boolean(string='Priority check', help='This field will be set "True" once it has been checked by the script')
 
     @api.depends('invisible_field')
     def make_invisible(self):
@@ -27,4 +32,19 @@ class Picking(models.Model):
             self.invisible_field = True
         else:
             self.invisible_field = False
+
+    def _zone_assignment(self):
+        for rec in self:
+            if 'PICK' in rec.name:
+                if rec.move_line_ids_without_package:
+                    move_id = rec.move_line_ids_without_package.location_id.location_id.ids
+                    print('ID del primer movimiento de almacen: ', move_id)
+                    rec.pick_zone = move_id[0]
+                    print(rec.pick_zone)
+                else:
+                    rec.pick_zone = None
+            else:
+
+                rec.pick_zone = None
+
 
