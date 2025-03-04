8f154456a344b6a684b9be146f1efb10a35bc81b
demianavilar@gmail.com <Demian Avila>
2025-03-04 15:41:40 +0100

    feat: added logs

--- wb_log/models/inherit_methods.py
+++ wb_log/models/inherit_methods.py
@@ -53,9 +53,10 @@ class WriteInLog(models.BaseModel):
                   # Only use threads if Odoo is fully initialized
                 """Override write method to use threading only when Odoo is fully loaded."""
                 _logger.info("------------------------------------")
-                _logger.info(vals)
-                _logger.info(self._fields.keys())
-                prev_record = self.read(list(self._fields.keys()))
+                #_logger.info(vals)
+                #_logger.info(self._fields.keys())
+                #prev_record = self.read(list(self._fields.keys()))
+                prev_record = self.read(list(vals.keys()))
                 _logger.info("prev record read")
                 result = super().write(vals)
                 new_record = self.read(list(self._fields.keys()))
